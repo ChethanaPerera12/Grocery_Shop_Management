28fe31125bbf8a1252d492421dcec3b8f59b2f3d
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-03-03 23:49:27 +0530

    Implement customer management functionality with CRUD operations

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,4 +1,36 @@
 package edu.icet.controller;
 
+import edu.icet.model.Customer;
+import edu.icet.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping("/get-all")
+    public List<Customer> getAll(){return customerService.getAll();
+    }
+    @PostMapping("/add-customer")
+    public boolean addCustomer(@RequestBody Customer customer){
+        return customerService.addCustomer(customer);
+    }
+    @PutMapping("/update-customer")
+    public boolean updateCustomer(@RequestBody Customer customer) {
+        return customerService.updateCustomer(customer);
+    }
+    @DeleteMapping("/delete-customer-id/{id}")
+    public boolean deleteCustomer(@PathVariable String id) {
+        return customerService.deleteCustomer(id);
+    }
+    @GetMapping("/search-customer-id/{id}")
+    public Customer searchById(@PathVariable String id) {
+        return customerService.searchById(id);
+    }
+
 }
--- src/main/java/edu/icet/model/Customer.java
+++ src/main/java/edu/icet/model/Customer.java
@@ -1,4 +1,16 @@
 package edu.icet.model;
 
+import java.util.Date;
+
 public class Customer {
+    private String id;
+    private String title;
+    private String name;
+    private Date dob;
+    private Double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String postalCode;
 }
+
