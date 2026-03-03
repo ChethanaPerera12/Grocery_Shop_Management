c7c4aba915759d2076741c6e81b03cea0f863395
Rasanjali-Perera <chethanaperera0912@gmail.com>
2026-03-04 01:21:52 +0530

    Implement CRUD operations in CustomerRepositoryImpl using JdbcTemplate

--- src/main/java/edu.icet/repository/Impl/CustomerRepositoryImpl.java
+++ src/main/java/edu.icet/repository/Impl/CustomerRepositoryImpl.java
@@ -2,33 +2,92 @@ package edu.icet.repository.Impl;
 
 import edu.icet.model.Customer;
 import edu.icet.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Override
     public List<Customer> getAll() {
-        return List.of();
+        String sql = "SELECT * FROM customer";
+        return jdbcTemplate.query(sql, (rs, rowNum) ->
+                new Customer(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDate(4),
+                        rs.getDouble(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getString(9)
+                )
+        );
     }
 
     @Override
     public boolean addCustomer(Customer customer) {
-        return false;
+        String sql = "INSERT INTO customer VALUES(?,?,?,?,?,?,?,?,?)";
+        return jdbcTemplate.update(sql,
+                customer.getId(),
+                customer.getTitle(),
+                customer.getName(),
+                customer.getDob(),
+                customer.getSalary(),
+                customer.getAddress(),
+                customer.getCity(),
+                customer.getProvince(),
+                customer.getPostalCode()
+        ) > 0;
     }
 
     @Override
     public boolean updateCustomer(Customer customer) {
-        return false;
+        String sql = "UPDATE customer SET CustTitle=?, CustName=?, DOB=?,salary=?, CustAddress=?, City=?, Province=?, PostalCode=? WHERE CustID=?";
+
+        return jdbcTemplate.update(sql,
+                customer.getTitle(),
+                customer.getName(),
+                customer.getDob(),
+                customer.getSalary(),
+                customer.getAddress(),
+                customer.getCity(),
+                customer.getProvince(),
+                customer.getPostalCode(),
+                customer.getId()
+        )>0;
+
     }
 
     @Override
     public boolean deleteCustomer(String id) {
-        return false;
+        String sql = "DELETE FROM customer WHERE CustID=?";
+        return jdbcTemplate.update(sql,id)>0;
     }
 
     @Override
     public Customer serchById(String id) {
-        return null;
+        String sql = "SELECT * FROM customer WHERE CustID=?";
+
+        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new Customer(
+                rs.getString(1),
+                rs.getString(2),
+                rs.getString(3),
+                rs.getDate(4),
+                rs.getDouble(5),
+                rs.getString(6),
+                rs.getString(7),
+                rs.getString(8),
+                rs.getString(9)
+        ), id);
+
     }
+
 }
+
